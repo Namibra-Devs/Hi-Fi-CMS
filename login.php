7ef8bb820e6c2717936419a6b48233b8d381a0a8
Charles Owusu Bih <charlesowusubih@gmail.com>
2023-06-22 01:06:34 +0000

    added the sweet alerts to handle login failed

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 //IMPORT THE DB CONN AND auxiliaries.phpS
 require_once "./conn.php";
 require_once "./auxiliaries.php";
-
+$alert = "";
 //CHECK IF THE SUBMIT BUTTON IS CLICKED
 if (isset($_POST['submit'])) {
     $email = $_POST['email'];
@@ -22,14 +22,17 @@ if (isset($_POST['submit'])) {
         //CHECK IF USER EMAIL IS EQUAL TO RETRIEVED EMAIL AND USER PASSWORD IS EQUAL TO RETRIEVED PASSWORD
         if ($email == $retrievedEmail && $password == $retrievedPassword) {
             //REDIRECT USER TO BLOG PAGE
+            $alert = "showAlert('success', 'Logged in succesfully')";
+            sleep(3);
             header("location: ./blog.php");
+            exit();
         } else {
             //SHOW ERROR TO USER
-            echo "wrong email or password";
+            $alert = "showAlert('error', 'wrong email or password')";
         }
     } else {
         //SHOW ERROR TO USER
-        echo "check your details";
+        $alert = "showAlert('error', 'Account Not found')";
     }
 }
 ?>
@@ -40,13 +43,25 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hi-Fi</title>
-    <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,900&display=swap" rel="stylesheet">
+    <!-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@7.12.15/dist/sweetalert2.all.min.js"></script>
+    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/sweetalert2@7.12.15/dist/sweetalert2.min.css'> -->
+    <link rel="stylesheet" href="style.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="./style.js"> </script>
+
+    </link>
+
 </head>
 
 <body>
+    <?php
+    echo "<script>";
+    echo $alert;
+    echo "</script>";
+    ?>
     <div class="container">
         <nav class="navbar">
             <a href="#">Company</a>
@@ -54,12 +69,12 @@ if (isset($_POST['submit'])) {
         <div class="login-container">
             <div class="form">
                 <h2>Admin Login</h2>
-                <form action="" method="POST">
+                <form action="" method="POST" autocomplete="off">
                     <div class="form-details">
-                        <label for="">Email</label>
-                        <input type="email" name="email" id="">
-                        <label for="">Password</label>
-                        <input type="password" name="password" id="">
+                        <label for="email">Email</label>
+                        <input type="email" name="email" id="email">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password">
                         <a href="#" style="text-align: right;">Forgot your password?</a>
                         <button type="submit" name="submit">Log In</button>
                     </div>
@@ -67,6 +82,7 @@ if (isset($_POST['submit'])) {
             </div>
         </div>
     </div>
+
 </body>
 
 </html>
