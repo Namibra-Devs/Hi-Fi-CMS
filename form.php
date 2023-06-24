21a77a8ccb0911b7f28b5cd056400e34aa03522c
Charles Owusu Bih <charlesowusubih@gmail.com>
2023-06-24 04:02:56 +0000

    changed  froala to tiny and added d&d input lib

--- form.php
+++ form.php
@@ -9,8 +9,12 @@
   <link rel="stylesheet" href="font-awesome-6/css/all.min.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link rel="stylesheet" href="./froala/froala_editor.pkgd.min.css">
+  <!-- <link rel="stylesheet" href="./froala/froala_editor.pkgd.min.css"> -->
+  <!-- FilePond styles -->
+  <link href="https://unpkg.com/filepond/dist/filepond.css" rel="stylesheet">
+
   <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,900&display=swap" rel="stylesheet">
+  <script src="https://cdn.tiny.cloud/1/b69kodqon0pasxedh64ci40757sso9c5b2jnz5dx5lv2un1o/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
 </head>
 
 <body>
@@ -40,13 +44,11 @@
           <div class="drag-image" id="dragger_wrapper">
             <h4>Blog Post Image</h4>
             <p>Shown in blog post overview and on the blog post page as a header Image</p>
-            <div id="dragger">
-              <div class="icon"><i class="fa fa-image"></i></div>
-              <h5 id="dragger_text">Drag and drop image here</h5>
-              <h4>Or</h4>
-              <button id="browseBtn">click browse file</button>
-              <input type="file" hidden id="fileSelectorInput">
+            <div>
+              <input type="file" multiple>
             </div>
+
+
           </div>
           <div class="drag-image" id="dragger_wrapper">
             <h4>Gallery</h4>
@@ -56,7 +58,7 @@
               <p>Add Image</p>
             </div>
           </div>
-          <textarea class="text-container" name="blogpost" id="froalaEditor" required>
+          <textarea class="text-container" name="blogpost" id="myTextarea" required>
 
           </textarea>
         </div>
@@ -65,9 +67,34 @@
   </div>
 
   <script src="style.js"></script>
-  <script type="text/javascript" src="./froala/froala_editor.pkgd.min.js"></script>
+  <!-- <script type="text/javascript" src="./froala/froala_editor.pkgd.min.js"></script>
   <script>
     var editor = new FroalaEditor('#froalaEditor');
+  </script> -->
+
+  <script>
+    //INITIALIZE TinyMCE AND ATTACH TO TEXTAREA ELEMENT
+    tinymce.init({
+      selector: "#myTextarea",
+      height: 400,
+      plugins: [
+        'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+        'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+        'insertdatetime', 'media', 'table', 'help', 'wordcount'
+      ],
+      toolbar: 'undo redo | blocks | ' +
+        'bold italic backcolor | alignleft aligncenter ' +
+        'alignright alignjustify | bullist numlist outdent indent |' +
+        'removeformat | help',
+      content_style: 'body { font-family: Helvetica, Arial, sans-serif; font-size:16px }'
+    });
+  </script>
+  <!-- ADD FILEPOND SCRIPTS -->
+  <script src="https://unpkg.com/filepond/dist/filepond.js"></script>
+  <script>
+    // SELECT AND CREATE THE FILEPOND ELEMENT
+    const inputElement = document.querySelector('input[type="file"]');
+    const pond = FilePond.create(inputElement);
   </script>
 </body>
 
